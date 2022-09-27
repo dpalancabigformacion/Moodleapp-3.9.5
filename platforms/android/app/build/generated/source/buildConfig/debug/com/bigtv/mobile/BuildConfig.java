1d9a440e4d2ee285bd0de8142920e4237d2dac3a
dpalancabigformacion <dpalanca@bigformacion.com>
2022-09-27 13:03:08 +0200

    BiGtv final

---
+++ platforms/android/app/build/generated/source/buildConfig/debug/com/bigtv/mobile/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.bigtv.mobile;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.bigtv.mobile";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 39503;
+  public static final String VERSION_NAME = "3.9.5";
+}
