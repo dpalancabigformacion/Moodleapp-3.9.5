722451a6d8fb1cd23757537bc7c7c3cf3643c09d
dpalancabigformacion <dpalanca@bigformacion.com>
2022-10-10 11:22:32 +0200

    api 31 ?¿

---
+++ build.js
@@ -0,0 +1,18 @@
+{
+    "android": {
+        "debug": {
+            "keystore": "./BiGtv.keystore",
+            "storePassword": "BiGtv.2022-2023",
+            "alias": "bigtv",
+            "password" : "BiGtv.2022-2023",
+            "keystoreType": "jks"
+        },
+        "release": {
+            "keystore": "./BiGtv.keystore",
+            "storePassword": "BiGtv.2022-2023",
+            "alias": "bigtv",
+            "password" : "BiGtv.2022-2023",
+            "keystoreType": "jks"
+        }
+    }
+}
